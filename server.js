a5b07059427b872eb61cb7cf41ea22fd4d78b30d
Luc van der Heiden <lucvdheiden@hotmail.com>
2022-03-08 17:54:25 +0100

    added games list

--- server.js
+++ server.js
@@ -2,9 +2,40 @@ const express = require('express')
 
 
 
-/* This is the object */
+/* Constants and variables */
 const app = express();
 const port = 3000;
+const movies = [
+        {
+            "id": 1,
+            "slug": "call-of-duty",
+            "name": "Call of Duty",
+            "year": "2021",
+            "categories": ["action", "fps"]
+        },
+        {
+            "id": 2,
+            "slug": "battlefield",
+            "name": "Battlefield",
+            "year": "2021",
+            "categories": ["action", "fps"]
+        },
+        {
+            "id": 3,
+            "slug": "rocket-league",
+            "name": "Rocket League",
+            "year": "2015",
+            "categories": ["sports", "party"]
+        },
+        {
+            "id": 4,
+            "slug": "f1-2021",
+            "name": "F1 2021",
+            "year": "2021",
+            "categories": ["sports", "racing"]
+        }
+
+]
 
 
 
