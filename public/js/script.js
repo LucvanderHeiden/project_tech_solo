deeb4cb23a191b002c69511f8f80aaf5486b16ab
Luc van der Heiden <lucvdheiden@hotmail.com>
2023-06-28 21:58:14 +0200

    implemented igdb api
    Replaced locally stored games with games from IGDB using their API.

--- public/js/script.js
+++ public/js/script.js
@@ -3,9 +3,18 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
+// form.addEventListener('submit', e => {
+//     validateInputs();
+// })
+
 form.addEventListener('submit', e => {
-    validateInputs();
-})
+    e.preventDefault(); // Prevent the default form submission behavior
+
+    if (validateInputs()) {
+        // The inputs are valid, allow the form submission
+        form.submit();
+    }
+});
 
 const scrollToError = () => {
     window.scroll({             /* Scroll automatisch naar boven na submit zodat de gebruiker eventuele error messages kan zien */
@@ -39,30 +48,104 @@ const isValidEmail = email => {
     return pattern.test(String(email).toLowerCase());
 }
 
+// const validateInputs = () => {
+//     const usernameValue = username.value.trim();            /* Trim zorgt ervoor dat alle whitespace van een string wordt weggehaald*/
+//     const emailValue = email.value.trim();
+//     const passwordValue = password.value.trim();
+
+//     if(usernameValue === '') {
+//         showError(username, 'Please choose a username')
+//     } else {
+//         showSuccess(username);
+//     }
+
+//     if(emailValue === '') {
+//         showError(email, 'Please fill in your email');
+//     } else if (!isValidEmail(emailValue)) {
+//         showError(email, 'Choose a valid email adress');
+//     } else {
+//         showSuccess(email);
+//     }
+
+//     if(passwordValue === '') {
+//         showError(password, 'Please choose a password');
+//     } else {
+//         showSuccess(password);
+//     }
+//  }
+
 const validateInputs = () => {
-    const usernameValue = username.value.trim();            /* Trim zorgt ervoor dat alle whitespace van een string wordt weggehaald*/
+    const usernameValue = username.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value.trim();
 
-    if(usernameValue === '') {
-        showError(username, 'Please choose a username')
+    if (usernameValue === '') {
+        showError(username, 'Please choose a username');
+        return false; // Prevent form submission
     } else {
         showSuccess(username);
     }
 
-    if(emailValue === '') {
+    if (emailValue === '') {
         showError(email, 'Please fill in your email');
+        return false; // Prevent form submission
     } else if (!isValidEmail(emailValue)) {
-        showError(email, 'Choose a valid email adress');
+        showError(email, 'Choose a valid email address');
+        return false; // Prevent form submission
     } else {
         showSuccess(email);
     }
 
-    if(passwordValue === '') {
+    if (passwordValue === '') {
         showError(password, 'Please choose a password');
+        return false; // Prevent form submission
     } else {
         showSuccess(password);
     }
- }
+
+    return true; // Allow form submission
+};
+
+const gameLabels = document.querySelectorAll('.game-label');
+
+gameLabels.forEach(label => {
+  label.addEventListener('click', () => {
+    const checkbox = label.previousElementSibling;
+    checkbox.checked = !checkbox.checked;
+    label.classList.toggle('checked');
+  });
+});
+
+const platformLabels = document.querySelectorAll('.platforms label');
+
+platformLabels.forEach(label => {
+  const checkbox = label.querySelector('input[type="checkbox"]');
+  const image = label.querySelector('img');
+
+  label.addEventListener('click', () => {
+    checkbox.checked = !checkbox.checked;
+    label.classList.toggle('checked');
+
+    if (checkbox.checked) {
+      image.classList.add('checked');
+    } else {
+      image.classList.remove('checked');
+    }
+  });
+
+  // Add event listener for touch devices
+  label.addEventListener('touchstart', () => {
+    checkbox.checked = !checkbox.checked;
+    label.classList.toggle('checked');
+
+    if (checkbox.checked) {
+      image.classList.add('checked');
+    } else {
+      image.classList.remove('checked');
+    }
+  });
+});
+
+
 
  console.log("script connected")
--- server.js
+++ server.js
@@ -1,42 +1,47 @@
 // Setup of packages (requires)
-const { generatePrimeSync } =   require('crypto');
-const express =                 require('express');
-const mongoose =                require('mongoose');
-const bodyParser =              require('body-parser')
-const User =                    require('./models/user') 
-const jwt =                     require('jsonwebtoken');
-const req =                     require('express/lib/request');
-const session =                 require('express-session');
-const passport =                require('passport');
-const flash =                   require('express-flash')
-const methodOverride =          require('method-override')
-const {checkAuthenticated, checkNotAuthenticated} = require('./middleware/authentification');
+const { generatePrimeSync } = require('crypto');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const User = require('./models/user');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('express-flash');
+const methodOverride = require('method-override');
+const { checkAuthenticated, checkNotAuthenticated } = require('./middleware/authentification');
+const axios = require('axios');
 require('dotenv').config();
 
 const initializePassport = require('./passport');
 const bcryptjs = require('bcryptjs');
 initializePassport(
-    passport,
-    async(username) => {
-        const userIsFound = await User.findOne({ username })
-        return userIsFound
-    },
-    async (id) => {
-        const userIsFound = await User.findOne({ _id: id });
-        return userIsFound;
-    }
+  passport,
+  async (username) => {
+    const userIsFound = await User.findOne({ username });
+    return userIsFound;
+  },
+  async (id) => {
+    const userIsFound = await User.findOne({ _id: id });
+    return userIsFound;
+  }
 );
+
+// IGDB API
+const clientId = 'ofsypygkrdlh25ri7tozs0s779pfm7';
+const clientSecret = 'lx5y6y7foc15y16fjzlnst3kfxlm44';
 
 // Express app
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(flash());
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET_CODE,
     resave: false,
     saveUninitialized: false,
-})
+  })
 );
 
 app.use(passport.initialize());
@@ -45,95 +50,196 @@ app.use(methodOverride('_method'));
 
 // Connect to MongoDB
 const dbURI = process.env.DATABASE_URI;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
-    .then((result) => console.log('connected to database'))             /* Console.log om te checken of er een succesvolle connectie met de database is */
-    .catch((err) => console.log(err))
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log('Connected to the database'))
+  .catch((err) => console.log(err));
 
 /* Register view engine */
-app.set('view engine', 'ejs');          /* Bron gebruikt voor het opzetten van EJS: https://www.youtube.com/watch?v=yXEesONd_54 (The Net Ninja) */
+app.set('view engine', 'ejs');
 
 // Port
-
 const port = process.env.PORT || 3000;
 
 /*********************************************************************
-Routes 
+Routes
 ***********************************************************************/
 
 app.get('/', checkNotAuthenticated, (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
+let accessToken; // Define the access token variable
+
+// Function to obtain the access token
+async function getAccessToken() {
+  try {
+    const response = await axios.post('https://id.twitch.tv/oauth2/token', null, {
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'client_credentials',
+      },
+    });
+    accessToken = response.data.access_token;
+  } catch (error) {
+    console.error('Error:', error.message);
+    throw error; // Throw the error to be caught by the calling function
+  }
+}
+
 app.get('/create', (req, res) => {
-    res.render('create');
+  const endpoint = '/games';
+  const fields = 'cover.url'; // Include the cover.url field
+  const limit = 20;
+  const sort = 'rating:desc'; // Sort the games by rating in descending order
+  const year2023Start = new Date('2023-01-01').getTime() / 1000;
+  const year2023End = new Date('2023-12-31').getTime() / 1000;
+  const year2022Start = new Date('2022-01-01').getTime() / 1000;
+  const year2022End = new Date('2022-12-31').getTime() / 1000;
+  const year2021Start = new Date('2021-01-01').getTime() / 1000;
+  const year2021End = new Date('2021-12-31').getTime() / 1000;
+
+  const headers = {
+    'Client-ID': clientId,
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  const body = `fields ${fields}; sort ${sort}; limit ${limit}; where rating > 0 & (first_release_date >= ${year2023Start} & first_release_date <= ${year2023End} | first_release_date >= ${year2022Start} & first_release_date <= ${year2022End} | first_release_date >= ${year2021Start} & first_release_date <= ${year2021End} & platforms = [167]);`;
+
+  console.log('Requesting games from the API...'); // Log a message before fetching games
+
+  getAccessToken() // Call the function to retrieve the access token
+    .then(() => {
+      axios
+        .post(`https://api.igdb.com/v4${endpoint}`, body, { headers })
+        .then((response) => {
+          const games = response.data;
+          console.log('Received games from the API:', games); // Log the games data to the console
+
+          res.render('create', { games });
+        })
+        .catch((error) => {
+          console.error('Error:', error.message);
+          res.status(500).send('An error occurred');
+        });
+    })
+    .catch((error) => {
+      console.error('Error:', error.message);
+      res.status(500).send('An error occurred');
+    });
 });
+
 
 // Dynamic profile page
 app.get('/profile', checkAuthenticated, (req, res) => {
-    res.render('profile', { user: req.user });
+  res.render('profile', { user: req.user });
 });
 
 // Recover account page (Doesn't have any actual functionality)
 app.get('/recover', (req, res) => {
-    res.render('recover');
+  res.render('recover');
 });
 
 app.post('/', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/profile',
-    failureRedirect: '/',
-    failureFlash: true
-}))
+  successRedirect: '/profile',
+  failureRedirect: '/',
+  failureFlash: true,
+}));
 
-app.post('/create', checkNotAuthenticated, async (req, res) => {
-    const userIsFound = await User.findOne({username: req.body.username})
-    const emailIsFound = await User.findOne({email: req.body.email})
+app.get('/create', (req, res) => {
+  const endpoint = '/games';
+  const fields = 'name,summary,cover.url'; // Include the cover.url field
+  const limit = 20;
+  const sort = 'rating:desc'; // Sort the games by rating in descending order
+  const year2023Start = new Date('2023-01-01').getTime() / 1000;
+  const year2023End = new Date('2023-12-31').getTime() / 1000;
+  const year2022Start = new Date('2022-01-01').getTime() / 1000;
+  const year2022End = new Date('2022-12-31').getTime() / 1000;
+  const year2021Start = new Date('2021-01-01').getTime() / 1000;
+  const year2021End = new Date('2021-12-31').getTime() / 1000;
 
-    if(userIsFound) {
-        req.flash('error', 'Username has already been taken')
-        res.redirect('/create');
-    } else if(emailIsFound) {
-        req.flash('error', 'Emailadress has already been taken')
-        res.redirect('/create');
-    } else {
-        try {
-            const passwordHash = await bcryptjs.hash(req.body.password, 10)
-            const user = new User({
-                username: req.body.username,
-                email: req.body.email,
-                password: passwordHash,
-                pc: req.body.pc,
-                playstation: req.body.playstation,
-                xbox: req.body.xbox,
-                fortnite: req.body.fortnite,
-                minecraft: req.body.minecraft,
-                gta: req.body.gta,
-                league: req.body.league,
-                f1: req.body.f1,
-                valorant: req.body.valorant,
-                rl: req.body.rl,
-                cod: req.body.cod,
-                fifa: req.body.fifa,
-                stardew: req.body.stardew
-            })
-            await user.save();
-            res.redirect('/');
-        } catch (error) {
-            console.log(error);
-        }
-    }
-})
+
+  const headers = {
+    'Client-ID': clientId,
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  const body = `fields ${fields}; sort ${sort}; limit ${limit}; where rating > 0 & (first_release_date >= ${year2023Start} & first_release_date <= ${year2023End} | first_release_date >= ${year2022Start} & first_release_date <= ${year2022End} | first_release_date >= ${year2021Start} & first_release_date <= ${year2021End} & platforms = [167]);`;
+
+  getAccessToken() // Call the function to retrieve the access token
+    .then(() => {
+      axios
+        .post(`https://api.igdb.com/v4${endpoint}`, body, { headers })
+        .then((response) => {
+          const games = response.data;
+          console.log(games); // Log the games data to the console
+
+          res.render('create', { games });
+        })
+        .catch((error) => {
+          console.error('Error:', error.message);
+          res.status(500).send('An error occurred');
+        });
+    })
+    .catch((error) => {
+      console.error('Error:', error.message);
+      res.status(500).send('An error occurred');
+    });
+});
+
 
 app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/')
-})
+  req.logOut();
+  res.redirect('/');
+});
 
 /* Error: 404 Page */
-app.use( (req, res) => {
-    res.status(404).send('Error 404: file not found')
-})
+app.use((req, res) => {
+  res.status(404).send('Error 404: file not found');
+});
+
+// Endpoint to obtain the access token
+app.get('/access-token', (req, res) => {
+  axios
+    .post('https://id.twitch.tv/oauth2/token', null, {
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'client_credentials',
+      },
+    })
+    .then((response) => {
+      const accessToken = response.data.access_token;
+
+      // Make a request to the IGDB API
+      axios
+        .get('https://api.igdb.com/v4/games', {
+          headers: {
+            'Client-ID': clientId,
+            Authorization: `Bearer ${accessToken}`,
+          },
+          params: {
+            fields: 'name,summary', // Specify the fields you want to retrieve
+            limit: 10, // Example: Limit the number of games to 10
+          },
+        })
+        .then((response) => {
+          const games = response.data;
+          res.json(games); // Send the games as a JSON response
+        })
+        .catch((error) => {
+          console.error('Error:', error.message);
+          res.status(500).send('An error occurred');
+        });
+    })
+    .catch((error) => {
+      console.error('Error:', error.message);
+      res.status(500).send('An error occurred');
+    });
+});
 
 /* Start webserver */
-app.listen(port, "0.0.0.0", function() {
-console.log("Web server running on port 3000");
-})
+app.listen(port, '0.0.0.0', function () {
+  console.log('Web server running on port 3000');
+});
